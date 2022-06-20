76827dd07fde34472461055a82fc86344bc91c80
lama <lamagt43@gmail.com>
2022-06-20 18:15:49 +0200

    added DeltaGamma params

--- packages/zircon-core/contracts/ZirconPylonFactory.sol
+++ packages/zircon-core/contracts/ZirconPylonFactory.sol
@@ -14,7 +14,12 @@ contract ZirconPylonFactory is IZirconPylonFactory {
 
     uint public maximumPercentageSync;
     uint public dynamicFeePercentage;
-    bytes4 private constant CREATE = bytes4(keccak256(bytes('createEnergy(address,address,address,address)')));
+
+    uint public deltaGammaThreshold;
+    uint public deltaGammaMinFee;
+
+
+    //bytes4 private constant CREATE = bytes4(keccak256(bytes('createEnergy(address,address,address,address)')));
 
     event PylonCreated(address indexed token0, address indexed token1, address poolToken0, address poolToken1, address pylon, address pair);
 
@@ -23,6 +28,9 @@ contract ZirconPylonFactory is IZirconPylonFactory {
         energyFactory = _energyFactory;
         maximumPercentageSync = 10;
         dynamicFeePercentage = 5;
+        deltaGammaThreshold = 4 * 1e16; //4%
+        deltaGammaMinFee = 1500; //15%
+
     }
 
     function allPylonsLength() external view returns (uint) {
