42f57e21cf062f12adaeb2cbc102a4196cc064d2
David Vittori <me@vittoridavid.com>
2022-11-02 18:01:58 +0100

    farming

--- packages/zircon-farming/scripts/check.js
+++ packages/zircon-farming/scripts/check.js
@@ -1,12 +1,12 @@
+const { ethers } = require('hardhat');
 
 async function check() {
     // Deploy Pylon Router
     let psionicFactory = await ethers.getContractFactory('PsionicFarmInitializable');
-    let psionicInstance = psionicFactory.attach("0x7CeEa7A00520F7f110314d177edE06EE9A3895d9")
-    // await psionicInstance.updatePylonRouter("0x385CC0c86DEf4dea78F6b3f0dBbfa3FB12FfecB5")
+    let psionicInstance = psionicFactory.attach("0xf26d1b3214388C17bDC4d20b3C2dE88310B0370E")
+    await psionicInstance.recoverToken("0x4545E94974AdACb82FC56BCf136B07943e152055")
     // let router = await psionicInstance.PYLON_ROUTER()
-    console.log("End Block: ", (await psionicInstance.bonusEndBlock()).toString())
-   
+    // console.log("End Block: ", (await psionicInstance.bonusEndBlock()).toString())
 }
 
 check()
